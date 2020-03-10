111928557c2e895cd566d4d4c06f55c7818f6cea
Jose M. Hidalgo-Lopez <53059588+Tatsinskaya@users.noreply.github.com>
2020-03-10 14:31:03 +0000

    update

--- scripts/Column_drop_test.py
+++ scripts/Column_drop_test.py
@@ -1,11 +1,15 @@
 from __future__ import print_function
 from sklearn.base import clone
 import pandas as pd
-#import numpy as np
-#from scipy.stats import linregress
-#from sklearn.tree import export_graphviz
-#from sklearn.ensemble import ExtraTreesRegressor
 import sys
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.dummy import DummyRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from scipy.stats import linregress
+
 sys.path.append('../inteql/')
 from modelFunctions import *
 
@@ -13,9 +17,10 @@ from modelFunctions import *
 def imp_df(column_names, importances):
     df = pd.DataFrame({'feature': column_names,
                        'feature_importance': importances}) \
-           .sort_values('feature_importance', ascending = False) \
-           .reset_index(drop = True)
+        .sort_values('feature_importance', ascending=False) \
+        .reset_index(drop=True)
     return df
+
 
 def drop_col_feat_imp(model, X_train, y_train, random_state=42):
     # clone the model to have the exact same specification as the one initially trained
@@ -35,25 +40,25 @@ def drop_col_feat_imp(model, X_train, y_train, random_state=42):
         model_clone.fit(X_train.drop(col, axis=1), y_train)
         drop_col_score = model_clone.score(X_train.drop(col, axis=1), y_train)
         importances.append(benchmark_score - drop_col_score)
-        print(col,importance, sep="\t")
+        print(col, importance, sep="\t")
     importances_df = imp_df(X_train.columns, importances)
     return importances_df
 
 
-finemap_file = sys.argv[1]      #'/nfs/research1/zerbino/jhidalgo/inteql/data/inter_data/output09.csv.gz'
-eqtl_file = sys.argv[2]         #'/nfs/research1/zerbino/jhidalgo/inteql/data/inter_data/output05.csv.gz'
-outfile = sys.argv[3]           # /nfs/research1/zerbino/jhidalgo/inteql/data/inter_data/output10_chr.csv.gz
+finemap_file = sys.argv[1]  # '/nfs/research1/zerbino/jhidalgo/inteql/data/inter_data/output09.csv.gz'
+eqtl_file = sys.argv[2]  # '/nfs/research1/zerbino/jhidalgo/inteql/data/inter_data/output05.csv.gz'
+outfile = sys.argv[3]  # /nfs/research1/zerbino/jhidalgo/inteql/data/inter_data/output10_chr.csv.gz
 
-#Finemap data
+# Finemap data
 data_z = pd.read_csv(finemap_file)
 eqtl = pd.read_csv(eqtl_file)
 data_z = data_z.merge(eqtl, on=['variant_id', 'gene_id']).drop_duplicates()
 data_z = data_z.fillna(0)
 data_z['z'] = data_z['z'].astype(float)
-data_z['Chromosome'] = data_z['variant_id'].str.split("_",1,expand=True)[0]
+data_z['Chromosome'] = data_z['variant_id'].str.split("_", 1, expand=True)[0]
 
-np.random.seed(seed = 42)
-data_z['random'] = np.random.random(size = len(data_z))
+np.random.seed(seed=42)
+data_z['random'] = np.random.random(size=len(data_z))
 
 # Get list of features names for each subset
 epigenomicFeatures = list(i for i in data_z.columns if i.startswith('enha') or i.startswith('prom'))[2:]
@@ -62,10 +67,10 @@ regbuild_activityFeatures = list(i for i in data_z.columns if i.startswith('acti
 regbuild_dataFeature = ['RegElementInfo']
 hiCFeatures = list(i for i in data_z.columns if i.startswith('hi'))
 eQTLFeatures = list(eqtl.columns[2:-1])
-distanceFeature = ['var_prom_distance','var_enh_distance']
+distanceFeature = ['var_prom_distance', 'var_enh_distance']
 chrFeature = ['Chromosome']
 
-X_label = epigenomicFeatures+hiCFeatures+eQTLFeatures+distanceFeature
+X_label = epigenomicFeatures + hiCFeatures + eQTLFeatures + distanceFeature
 X = data_z[X_label]
 y = data_z['z']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
@@ -81,9 +86,7 @@ importances = list(model.feature_importances_)
 feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
 feature_importances = sorted(feature_importances, key=lambda x: x[1], reverse=True)
 
-with open(outfile,'w+') as f:
-    [print('Variable: {:20} Importance: {}'.format(*pair),file=f) for pair in feature_importances]
+with open(outfile, 'w+') as f:
+    [print('Variable: {:20} Importance: {}'.format(*pair), file=f) for pair in feature_importances]
 
 drop_col_feat_imp(model)
-
-
