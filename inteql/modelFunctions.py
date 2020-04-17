75c85ab09ecfb77974995c4c001d8cac0bebdb0d
Jose M. Hidalgo-Lopez <53059588+Tatsinskaya@users.noreply.github.com>
2020-04-17 09:07:39 +0100

    update

--- inteql/modelFunctions.py
+++ inteql/modelFunctions.py
@@ -25,25 +25,27 @@ def decision_tree_regressor(data, X_label, y_label, random_state, test_size=0.3,
         - 'model': The trained model
         - 'y_pred': The predictions of the test set
     """
-    
+
     # Split data
     X = data[X_label]
     y = data[y_label]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
-    
+
     # print(X_train.shape)
     # Create a model and train it
     model = DecisionTreeRegressor(random_state=random_state, max_depth=max_depth)
     model = model.fit(X_train, y_train)
-    
+
     # Predict values for test set and asses error
     y_pred = model.predict(X_test)
     rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-    
-    # Return result object
-    return {'rmse':rmse, 'model':model, 'y_pred':y_pred}
 
-def random_forest_regressor(data, X_label, y_label, random_state, test_size=0.3, max_depth=83, n_estimators=136, min_samples_split=2, min_samples_leaf=1, max_features='sqrt'):
+    # Return result object
+    return {'rmse': rmse, 'model': model, 'y_pred': y_pred}
+
+
+def random_forest_regressor(data, X_label, y_label, random_state, test_size=0.3, max_depth=83, n_estimators=136,
+                            min_samples_split=2, min_samples_leaf=1, max_features='sqrt'):
     """
     This function generate a Random Forest Regressor. 
     First it splits the data into train and test. 
@@ -65,17 +67,16 @@ def random_forest_regressor(data, X_label, y_label, random_state, test_size=0.3,
         - 'r_value': The R value of the regression line
         - 'y_test': The target column of the test set
     """
-    
+
     # Split data
     X = data[X_label]
     y = data[y_label]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
-    
+
     # Create a model and train it
     model = RandomForestRegressor(random_state=random_state, max_depth=max_depth, n_estimators=n_estimators)
     model = model.fit(X_train, y_train)
-    
-        
+
     # Predict values for test set and asses error
     y_pred = model.predict(X_test)
     rmse = np.sqrt(mean_squared_error(y_test, y_pred))
@@ -83,7 +84,9 @@ def random_forest_regressor(data, X_label, y_label, random_state, test_size=0.3,
     importances = list(model.feature_importances_)
     feature_list = list(X.columns)
     # Return result object
-    return {'rmse':rmse, 'model':model, 'y_pred':y_pred, 'r_value':r_value, 'y_test':y_test, 'importances':importances,'features':feature_list}
+    return {'rmse': rmse, 'model': model, 'y_pred': y_pred, 'r_value': r_value, 'y_test': y_test,
+            'importances': importances, 'features': feature_list}
+
 
 def dummy_regressor(data, X_label, y_label, random_state, test_size=0.3):
     """
@@ -105,19 +108,64 @@ def dummy_regressor(data, X_label, y_label, random_state, test_size=0.3):
         - 'model': The trained model
         - 'y_pred': The predictions of the test set
     """
-    
+
     # Split data
     X = data[X_label]
     y = data[y_label]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
-    
+
     # Create a model and train it
     model = DummyRegressor()
     model = model.fit(X_train, y_train)
-    
+
     # Predict values for test set and asses error
     y_pred = model.predict(X_test)
     rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 
     # Return result object
     return {'rmse': rmse, 'model': model, 'y_pred': y_pred}
+
+
+def random_forest_regressor_fixed_split(traindata, testdata, X_label, y_label, max_depth=83, n_estimators=136,
+                                        min_samples_split=2, min_samples_leaf=1, max_features='sqrt'):
+    """ todo rewrite doc
+    This function generate a Random Forest Regressor.
+    This variant uses predefined test and train data instead of splitting randomly.
+    Then, trains the model.
+    Finally, predicts the y values for the test set and compute its error.
+    It also computes the regression line for the test and predictions.
+    It receives:
+        - Data: as a data frame
+        - List of the feature column names: list
+        - Target column name: str
+        - Random state: float
+        - The size of the test set: float [0,1]
+        - The maximum depth of the tree: int
+        - The number of estimators: int. By default is 100.
+    It returns a dictionary:
+        - 'rmse': The root mean square error of the predictor
+        - 'model': The trained model
+        - 'y_pred': The predictions of the test set
+        - 'r_value': The R value of the regression line
+        - 'y_test': The target column of the test set
+    """
+
+    # Split data
+    X_train = traindata[X_label]
+    y_train = traindata[y_label]
+    X_test = testdata[X_label]
+    y_test = testdata[y_label]
+
+    # Create a model and train it
+    model = RandomForestRegressor(random_state=random_state, max_depth=max_depth, n_estimators=n_estimators)
+    model = model.fit(X_train, y_train)
+
+    # Predict values for test set and asses error
+    y_pred = model.predict(X_test)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    slope, intercept, r_value, p_value, std_err = linregress(y_test, y_pred)
+    importances = list(model.feature_importances_)
+    feature_list = list(X.columns)
+    # Return result object
+    return {'rmse': rmse, 'model': model, 'y_pred': y_pred, 'r_value': r_value, 'y_test': y_test,
+            'importances': importances, 'features': feature_list}
